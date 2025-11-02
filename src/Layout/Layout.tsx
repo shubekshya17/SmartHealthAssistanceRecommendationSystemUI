37d604db69466a2ae8ca93de375ed58fd5323b62
Sudipa Amgai <100207556+sudipaamgai@users.noreply.github.com>
2025-11-02 15:12:09 +0545

    Revert "Chatbot integration"

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
-//import Navbar from "../Components/Navbar";
-import ChatBot from '../Components/Chatbot';
 
 const { Content } = Layout;
 
@@ -10,23 +8,9 @@ const WebsiteLayout: React.FC = () => {
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      {/* <Header style={{ display: "flex", alignItems: "center" }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          items={WebsiteMenuItems.map((item) => ({
-            key: item.key,
-            label: item.label,
-          }))}
-          style={{ flex: 1, minWidth: 0, padding: "0px 2px" }}
-        />
-      </Header> */}
       <Content>
           <Outlet />
       </Content>
-      <ChatBot />
-
     </Layout>
   );
 };
